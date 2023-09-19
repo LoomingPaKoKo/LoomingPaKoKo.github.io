6f00feb6eadb6f6b2826fd3333203e3a07130908
PaKoKo <2830717394@qq.com>
2023-09-20 00:23:32 +0800

    Site updated: 2023-09-20 00:23:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/17/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/17/hello-world/","2023/09/19/test/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
